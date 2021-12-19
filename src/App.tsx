acc01bb8d39d31c2edf4eca6241e29c027c5c170
ifizzyou <pjh15237213@gmail.com>
2021-12-19 14:09:26 +0900

    flix 1219

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import styled from "styled-components";
-
-const Wrapper = styled.div`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const Box = styled.div`
-  width: 200px;
-  height: 200px;
-  background-color: white;
-  border-radius: 10px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
 function App() {
   return (
-    <Wrapper>
-      <Box />
-    </Wrapper>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+        <Route path={["/", "/movies/:movieId"]}>
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
